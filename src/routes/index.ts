41fe65528934abb79d851ea5fa0a11e3b5ccdd1b
lijovijayan <lijovijayan00@gmail.com>
2021-12-09 15:09:28 +0530

    url prefix fix

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -9,12 +9,12 @@ import students from '@routes/student.route';
 
 const routes = Router();
 
-routes.use('oneshot/api/v1/countries', countries);
-routes.use('oneshot/api/v1/states', states);
-routes.use('oneshot/api/v1/cities', cities);
-routes.use('oneshot/api/v1/cources', cources);
-routes.use('oneshot/api/v1/skills', skills);
-routes.use('oneshot/api/v1/colleges', colleges);
-routes.use('oneshot/api/v1/students', students);
+routes.use('/oneshot/api/v1/countries', countries);
+routes.use('/oneshot/api/v1/states', states);
+routes.use('/oneshot/api/v1/cities', cities);
+routes.use('/oneshot/api/v1/cources', cources);
+routes.use('/oneshot/api/v1/skills', skills);
+routes.use('/oneshot/api/v1/colleges', colleges);
+routes.use('/oneshot/api/v1/students', students);
 
 export default routes;
